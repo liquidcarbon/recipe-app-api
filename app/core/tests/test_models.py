b04e755d1ba88bdf7e1848f11d98a5421974af2e
Alexander Kislukhin <akscrps@gmail.com>
2020-08-14 13:37:24 -0600

    linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -15,7 +16,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_normalized(self):
         """Test the email for a new user is normalized."""
         email = 'test@foo.moo'
@@ -23,12 +23,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creating user with no email."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 't')
-
 
     def test_create_new_superuser(self):
         """Test creating superuser."""
